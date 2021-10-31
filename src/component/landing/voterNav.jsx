af2181e515eba3868ec9446b01bf44952b59624f
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-10-31 19:36:25 +0700

    add dsc logo to navbar

--- src/component/landing/voterNav.jsx
+++ src/component/landing/voterNav.jsx
@@ -1,14 +1,30 @@
 import './landing.scss';
+import Logo from '../../image/logo-dsc-color-horizontal.png';
 import { Link } from "react-router-dom";
 
 export function VoterNav({brand, accountOrganizer, documentation, ...props}){
     return(
         <div>
             <ul className='ultra'>
-                <li className='list-left'><Link to='' className='brand'>{brand}</Link></li>
-                <li className='list-right'><Link to='/organisasi'>{accountOrganizer}</Link></li>
-                <li className='list-right'><Link to='/documentation'>{documentation}</Link></li> 
-                <li className='list-right'><Link to='/'>{props.landing}</Link></li> 
+                <li className='list-left'>
+                    <img 
+                        src={Logo} 
+                        alt='logo dsc unjani'
+                        height={50}
+                    />
+                </li>
+                <li className='list-left'>
+                    <Link to='' className='brand'>{brand}</Link>
+                </li>
+                <li className='list-right'>
+                    <Link to='/organisasi'>{accountOrganizer}</Link>
+                </li>
+                <li className='list-right'>
+                    <Link to='/documentation'>{documentation}</Link>
+                </li> 
+                <li className='list-right'>
+                    <Link to='/'>{props.landing}</Link>
+                </li> 
             </ul>
         </div>
     )
